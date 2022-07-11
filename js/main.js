09ed4e032d76f2d241560f166f62a4f4c751cc89
daidensacha <daidensacha@gmail.com>
2022-07-11 22:16:46 +0200

    update style and js

--- js/main.js
+++ js/main.js
@@ -2,41 +2,40 @@ function getRandomInt(num) {
   return Math.floor(Math.random() * num.length);
 }
 
-const menuItems = {
+const meals = {
   breakfast: ['yoghurt with fruit', 'toast with butter and choice of jam', 'porridge', 'croisant with jam', 'English breakfast', 'bacon and eggs', 'fasting with fresh water'],
   lunch: ['ham and salad sandwich', 'meat pie', 'bbq steak and sausages', 'thai noodles', 'chicken salad', 'fresh fruit', 'garden salad with feta cheese'],
-  dinner: ['lasagna', 'roast pork with vegetables', 'pizza', 'cheese and crsackers', 'chicken nachos', 'veal schnitzel with salad', 'pesto pasta']
-}
+  dinner: ['lasagna', 'roast pork with vegetables', 'pizza', 'cheese and crackers', 'chicken nachos', 'veal schnitzel with salad', 'pesto pasta']
+};
 
 const randomMenu = {
   breakfast: '',
   lunch: '',
   dinner: ''
-}
-
+};
 const menuLotto = obj => {
 
-  for (meal in menuItems) {
+  for (let meal in obj) {
 
-    let randomNum = getRandomInt(meal);
-    console.log(`array length = ${randomNum}`);
+    let randomNum = getRandomInt(meals[meal]);
 
     if (meal === 'breakfast') {
-      // console.log(menuItems.breakfast[randomNum])
-      console.log(randomNum)
-      randomMenu.breakfast = `Breakfast today is <span class="meal">${menuItems.breakfast[randomNum]}</span>.`;
+
+      randomMenu.breakfast = `Breakfast today is <span class="meal">${meals.breakfast[randomNum]}</span>.`;
       document.getElementById('breakfast').innerHTML = randomMenu.breakfast;
     } else if (meal === 'lunch') {
-      randomMenu.lunch = `For lunch <span class="meal">${menuItems.lunch[randomNum]}</span> will be served.`;
+      randomMenu.lunch = `For lunch <span class="meal">${meals.lunch[randomNum]}</span> will be served.`;
       document.getElementById('lunch').innerHTML = randomMenu.lunch;
 
     } else if (meal === 'dinner') {
-      randomMenu.dinner = `For dinner, <span class="meal">${menuItems.dinner[randomNum]}</span> is on the menu.`;
+      randomMenu.dinner = `For dinner, <span class="meal">${meals.dinner[randomNum]}</span> is on the menu.`;
       document.getElementById('dinner').innerHTML = randomMenu.dinner;
     }
   }
 
   return randomMenu;
-}
+};
 
-menuLotto(menuItems);
+menuLotto(meals);
+
+document.getElementById('refresh-menu').addEventListener('click', () => menuLotto(meals));
